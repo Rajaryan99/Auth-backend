f980dd0f119db18964868a4fa06d8d57cf6b3143
Raj Aryan <raj.aryan2518@gmail.com>
2026-04-02 02:15:23 +0530

    server created for node App

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 import express from "express";
 import 'dotenv/config'
+import mongoose from "mongoose";
 
 const app = express()
 
@@ -11,4 +12,5 @@ app.get("/", (req, res) => {
 
 app.listen(port, () => {
     console.log(`server is running on http://localhost:${port}`)
+    
 })
