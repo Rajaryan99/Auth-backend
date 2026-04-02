05d3c114e6227c3ea007773a52ec087ca967a165
Raj Aryan <raj.aryan2518@gmail.com>
2026-04-02 20:35:53 +0530

    jwt checks for authentication

--- app.js
+++ app.js
@@ -11,12 +11,16 @@ const port  = process.env.PORT || 3000;
 app.use(express.json())
 app.use(cors())
 
-app.use('/api/auth', authRouter)
+app.use('/api/auth', authRouter);
 
 app.get("/", (req, res) => {
     res.send("Hello World!!!")
 })
 
+
+if(!process.env.JWT_SECRET){
+    throw new Error("JWT_SECRET is not defined in enviroment variable")
+}
 
 
 const connectDB = async () => {
